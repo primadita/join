d24bdb88eff64b336a21d96c334b6a7932c04ed4
Nils Alberding-Schulz <nilsalberding@web.de>
2025-09-29 15:27:44 +0200

    integrate navbar into main

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ContactsComponent } from './contacts/contacts.component';
 import { NavBarComponent } from '../shared/components/nav-bar/nav-bar.component';
+import { HeaderComponent } from '../shared/components/header/header.component';
 
 @Component({
   selector: 'app-main-content',
-  imports: [ContactsComponent, NavBarComponent],
+  imports: [ContactsComponent, NavBarComponent, HeaderComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
