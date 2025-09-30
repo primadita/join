7c7d11ec6f695752426b2b28b022c9eea6427bbe
Henry Wolf <HWolf.binz@gmail.com>
2025-09-30 10:50:00 +0200

    feat: added user-list dummy data to contact-list

--- src/app/main-content/contacts/contact-list/contact-label/contact-label.component.ts
+++ src/app/main-content/contacts/contact-list/contact-label/contact-label.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-contact-label',
   imports: [],
   templateUrl: './contact-label.component.html',
-  styleUrl: './contact-label.component.scss'
+  styleUrl: './contact-label.component.scss',
 })
 export class ContactLabelComponent {
-
+  @Input() user: any;
 }
--- src/app/main-content/contacts/contact-list/contact-list.component.ts
+++ src/app/main-content/contacts/contact-list/contact-list.component.ts
@@ -1,11 +1,20 @@
+// contact-list.component.ts
 import { Component } from '@angular/core';
 import { users } from '../../../shared/userData';
 import { ContactLabelComponent } from './contact-label/contact-label.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact-list',
-  imports: [ContactLabelComponent],
+  standalone: true,
+  imports: [CommonModule, ContactLabelComponent],
   templateUrl: './contact-list.component.html',
   styleUrl: './contact-list.component.scss',
 })
-export class ContactListComponent {}
+export class ContactListComponent {
+  users = users;
+
+  constructor() {
+    console.log(this.users);
+  }
+}
