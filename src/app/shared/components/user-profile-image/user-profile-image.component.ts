514c630f67e37ba6c60c83f6f5c7f45f99ccb3f7
primadita <pulomas10@hotmail.com>
2025-09-30 14:21:52 +0200

    feat: user profile image

--- src/app/shared/components/user-profile-image/user-profile-image.component.ts
+++ src/app/shared/components/user-profile-image/user-profile-image.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NgStyle } from "../../../../../node_modules/@angular/common/common_module.d-NEF7UaHr";
+import { FirebaseServiceService } from '../../services/firebase.service';
+import { UserProfileImageService } from '../../services/user-profile-image.service';
+import { Contact } from '../../interfaces/contact';
 
 @Component({
   selector: 'app-user-profile-image',
-  imports: [],
+  imports: [NgStyle],
   templateUrl: './user-profile-image.component.html',
   styleUrl: './user-profile-image.component.scss'
 })
 export class UserProfileImageComponent {
-
+  @Input() initial: string = '';
+  @Input() bgColor: string = '';
 }
