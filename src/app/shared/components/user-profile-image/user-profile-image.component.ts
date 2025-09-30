261c579338c7c3234b830768edb63691d79c95ce
primadita <pulomas10@hotmail.com>
2025-09-30 14:25:23 +0200

    feat: user profile image

--- src/app/shared/components/user-profile-image/user-profile-image.component.ts
+++ src/app/shared/components/user-profile-image/user-profile-image.component.ts
@@ -1,12 +1,13 @@
 import { Component, Input } from '@angular/core';
-import { NgStyle } from "../../../../../node_modules/@angular/common/common_module.d-NEF7UaHr";
+// import { CommonModule, NgStyle } from "../../../../../node_modules/@angular/common/common_module.d-NEF7UaHr";
 import { FirebaseServiceService } from '../../services/firebase.service';
 import { UserProfileImageService } from '../../services/user-profile-image.service';
 import { Contact } from '../../interfaces/contact';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-profile-image',
-  imports: [NgStyle],
+  imports: [CommonModule],
   templateUrl: './user-profile-image.component.html',
   styleUrl: './user-profile-image.component.scss'
 })
