fe8e3c0268612a5d237ca63570b1d4dfc34dd14a
Nils Alberding-Schulz <nilsalberding@web.de>
2025-09-30 20:20:59 +0200

    feat: add form validation

--- src/app/main-content/contacts/add-contact/add-contact.component.ts
+++ src/app/main-content/contacts/add-contact/add-contact.component.ts
@@ -1,11 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-contact',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './add-contact.component.html',
   styleUrl: './add-contact.component.scss'
 })
 export class AddContactComponent {
+
+    contactData = {
+    name: "",
+    email: "",
+    phone: ""
+  };
+
+
 
 }
