42b284086ceb17230d8d3ad03a42fd49e3eca041
Henry Wolf <HWolf.binz@gmail.com>
2025-09-30 14:45:53 +0200

    feat: enhanced contact label component with email display and styling

--- src/app/main-content/contacts/contact-list/contact-label/contact-label.component.ts
+++ src/app/main-content/contacts/contact-list/contact-label/contact-label.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FirebaseServiceService } from '../../../../shared/services/firebase.service';
+import { Contact } from '../../../../shared/interfaces/contact';
 
 @Component({
   selector: 'app-contact-label',
   imports: [],
   templateUrl: './contact-label.component.html',
-  styleUrl: './contact-label.component.scss'
+  styleUrl: './contact-label.component.scss',
 })
 export class ContactLabelComponent {
+  contactList = inject(FirebaseServiceService);
 
+  getLetters(contact: Contact): string {
+    const parts = contact.name.trim().split(' ');
+    const initials = parts.map((user) => user.charAt(0).toUpperCase()).join('');
+    return initials;
+  }
 }
