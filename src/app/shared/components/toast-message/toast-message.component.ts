77c469fb7661f31b7040de672b4e3b1e016c3700
primadita <pulomas10@hotmail.com>
2025-10-04 01:14:53 +0200

    feat: add toast mesaage component

--- src/app/shared/components/toast-message/toast-message.component.ts
+++ src/app/shared/components/toast-message/toast-message.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ToastMessagesService } from '../../services/toast-messages.service';
+import { CommonModule } from '@angular/common';
+import { Messages } from '../../interfaces/messages';
 
 @Component({
   selector: 'app-toast-message',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './toast-message.component.html',
   styleUrl: './toast-message.component.scss'
 })
 export class ToastMessageComponent {
+  message: Messages | null = null;
 
+  constructor(private toastService: ToastMessagesService){}
+
+  ngOnInit(){
+    this.toastService.toastStatus.subscribe(msg => {
+      this.message = msg;
+      setTimeout(() => this.message = null, 3000);
+    });
+  }
 }
