9da0a2eff7c3cd311ba9d079fb9cfc6c2de104df
Nils Alberding-Schulz <nilsalberding@web.de>
2025-10-09 15:10:04 +0200

    add components for sprint 2

---
+++ src/app/main-content/add-task-view/add-task-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-task-view',
+  imports: [],
+  templateUrl: './add-task-view.component.html',
+  styleUrl: './add-task-view.component.scss'
+})
+export class AddTaskViewComponent {
+
+}
---
+++ src/app/main-content/board/add-task-popup/add-task-popup.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-task-popup',
+  imports: [],
+  templateUrl: './add-task-popup.component.html',
+  styleUrl: './add-task-popup.component.scss'
+})
+export class AddTaskPopupComponent {
+
+}
