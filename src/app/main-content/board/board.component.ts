ff1d4609fd371ee01d27aa06bcad10c41ab8eb5b
primadita <pulomas10@hotmail.com>
2025-10-14 06:11:41 +0200

    feat: progress board with drag-drop function to test

--- src/app/main-content/board/board.component.ts
+++ src/app/main-content/board/board.component.ts
@@ -1,11 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { CdkDrag, CdkDragDrop, CdkDropList, DragDropModule, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop' ;
+import { TaskCardComponent } from './task-card/task-card.component';
 @Component({
   selector: 'app-board',
-  imports: [],
+  imports: [CommonModule, DragDropModule, TaskCardComponent, CdkDropList, CdkDrag],
   templateUrl: './board.component.html',
   styleUrl: './board.component.scss'
 })
 export class BoardComponent {
-
+  todo = [];
+  inprogress = [];
+  
+  drop(event: CdkDragDrop<string[]>){
+    if(event.previousContainer === event.container){
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    }else{
+      transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex);
+    }
+  }
 }
