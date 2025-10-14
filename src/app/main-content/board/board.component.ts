99ca19c0394326784841637c1b8999eade021d4b
Henry Wolf <HWolf.binz@gmail.com>
2025-10-14 08:24:51 +0200

    feat: implement task details view and interaction in board component

--- src/app/main-content/board/board.component.ts
+++ src/app/main-content/board/board.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { TaskCardComponent } from './task-card/task-card.component';
+import { TaskDetailsComponent } from './task-card/task-details/task-details.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-board',
-  imports: [TaskCardComponent],
+  imports: [CommonModule, TaskCardComponent, TaskDetailsComponent],
   templateUrl: './board.component.html',
-  styleUrl: './board.component.scss'
+  styleUrl: './board.component.scss',
 })
 export class BoardComponent {
+  showDetail = false;
 
+  openTask() {
+    this.showDetail = true;
+  }
+
+  closeTask() {
+    this.showDetail = false;
+  }
 }
