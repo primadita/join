952261af847e66859e062fab459615f60439124c
Henry Wolf <HWolf.binz@gmail.com>
2025-10-17 08:54:35 +0200

    feat: add comments

--- src/app/shared/services/task.service.ts
+++ src/app/shared/services/task.service.ts
@@ -1,4 +1,4 @@
-import { inject, Injectable, OnDestroy } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import {
   addDoc,
   collection,
@@ -7,19 +7,21 @@ import {
   deleteDoc,
   doc,
   Firestore,
-  onSnapshot,
-  Unsubscribe,
   updateDoc,
 } from '@angular/fire/firestore';
-import { Title } from '@angular/platform-browser';
 import { Task } from '../interfaces/task';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 
+/*
+  Der Service lebt, so lange wie die App
+  ngOnDestroy() wird daher bei ihnen nicht aufgerufen
+*/
 @Injectable({
   providedIn: 'root',
 })
 
-/* Ein Service ist eine Hilfsklasse, die Dinge erledigt, die mehrere Komponenten brauchen
+/* 
+    Ein Service ist eine Hilfsklasse, die Dinge erledigt, die mehrere Komponenten brauchen
      z.B. Daten holen, speichern usw.
   
   */
@@ -28,22 +30,22 @@ export class TaskService {
     Firestore ist eine Klasse, die von AngularFire kommt.
     Angular erzeugt ein Firestore-Objekt / Instanz und speichert es in fs.
     Das nennt man Dependency Injection
-    Durch inject kann man nun fs außerhalb des constructor benutzen
-    protected verhindert Zugriff von außen
+    Durch inject kann man nun fs außerhalb des constructors benutzen
+    protected verhindert Zugriff von außen - nur TaskService & eventuelle Unterklassen können es sehen und nutzen
   */
   protected fs: Firestore = inject(Firestore);
 
   /*
     Dies erstellt einen Verweis (Reference) auf die Firestore-Sammlung
-    Durch private kann keine komponente direkt auf Collection zugreifen
+    Durch private kann keine Komponente direkt auf die Collection zugreifen
     sie können nur fertige Methoden verwenden die auch public sind
   */
   private tasksCol = collection(this.fs, 'tasks') as CollectionReference<Task>;
 
   /*
     collectionData(...) gibt ein Observable - aktualisiert sich automatisch
-    idField - damit man weiß, welches Dokument man aktualisieren oder löschen möchte
-    readonly - tasks$ wird niemals überschrieben sondern nur nutzen
+    idField ist damit man weiß, welches Dokument man aktualisieren oder löschen möchte
+    readonly - tasks$ wird niemals überschrieben sondern nur genutzt
   */
   readonly tasks$: Observable<Task[]> = collectionData(this.tasksCol, {
     idField: 'id',
@@ -56,7 +58,7 @@ export class TaskService {
 
   /**
    * Nimmt das Task interface aber lässt das Feld "id" weg
-   * Denn wenn ein neues Dokument angelegt wird, gibt es dies ID noch nicht
+   * Denn wenn ein neues Dokument angelegt wird, gibt es diese ID noch nicht
    * Firestore vergibt diese Automatisch
    *
    * @return promise - DocumentReference-Object
@@ -81,7 +83,7 @@ export class TaskService {
 
   /**
    * Löscht das Dokument mit dieser ID aus der Task-Colletion
-   * doc(...) ist wieder eine Reference auf diese Dokument in Firestore
+   * doc(...) ist wieder eine Reference auf dieses Dokument in Firestore
    * deleteDoc(...) löscht dieses Dokument aus der Datenbank
    * @param id
    * @returns promise<void> - kein Ergebnis, nur das Signal "fertig"
