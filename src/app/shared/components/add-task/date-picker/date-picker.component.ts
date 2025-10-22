7bd5c7c2221b958946df5e88da78b96fa392a6ba
Nils Alberding-Schulz <nilsalberding@web.de>
2025-10-22 10:26:23 +0200

    fix: date now in us-format

--- src/app/shared/components/add-task/date-picker/date-picker.component.ts
+++ src/app/shared/components/add-task/date-picker/date-picker.component.ts
@@ -3,28 +3,15 @@ import { CommonModule } from '@angular/common';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { provideNativeDateAdapter, MatNativeDateModule, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+import { provideNativeDateAdapter, MatNativeDateModule } from '@angular/material/core';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { FormsModule } from '@angular/forms';
 
-
-const MY_DATE_FORMATS = {
-  parse: {
-    dateInput: 'DD/MM/YYYY', // Eingabeformat (manuelles Tippen)
-  },
-  display: {
-    dateInput: 'dd/MM/yyyy', // Anzeigeformat im Input
-    monthYearLabel: 'MMM yyyy',
-    dateA11yLabel: 'dd/MM/yyyy',
-    monthYearA11yLabel: 'MMMM yyyy',
-  },
-};
 @Component({
   selector: 'app-date-picker',
   providers: [
     provideNativeDateAdapter(),
-    { provide: MAT_DATE_LOCALE, useValue: 'fr-FR' },
-    { provide: MAT_DATE_FORMATS, useValue: MY_DATE_FORMATS },
+
   ],
   imports: [
     CommonModule,
@@ -33,6 +20,7 @@ const MY_DATE_FORMATS = {
     MatFormFieldModule,
     MatAutocompleteModule,
     FormsModule,
+    MatNativeDateModule
   ],
   templateUrl: './date-picker.component.html',
   styleUrl: './date-picker.component.scss'
