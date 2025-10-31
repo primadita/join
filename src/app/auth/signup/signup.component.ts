ae1d43ea8abc75570c22fa843122ddc95bba1495
primadita <pulomas10@hotmail.com>
2025-10-31 13:52:36 +0100

    feat: form validation for sign up form

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../../shared/components/footer/footer.component';
 import { ToastMessageComponent } from '../../shared/components/toast-message/toast-message.component';
+import { FormsModule, NgForm } from '@angular/forms';
+import { PatternValidatorDirective } from '../../shared/directives/pattern-validator.directive';
+import { ToastMessagesService } from '../../shared/services/toast-messages.service';
 
 @Component({
   selector: 'app-signup',
-  imports: [FooterComponent, ToastMessageComponent],
+  imports: [FooterComponent, ToastMessageComponent, FormsModule, PatternValidatorDirective],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
+  // #region ATTRIBUTES
+  user = {
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: ""
+  }
+  // #endregion
 
+  constructor(private toastService: ToastMessagesService){}
+  // #region METHODS
+  onSubmit(ngForm: NgForm){
+    this.toastService.show('Sign up is successful',"success");
+  }
+  // #endregion
 }
