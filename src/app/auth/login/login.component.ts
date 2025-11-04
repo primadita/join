03804280015c43c6bd611f9fd0b0b0eed86611ba
Henry Wolf <HWolf.binz@gmail.com>
2025-11-04 12:19:27 +0100

    add: login as guest now possible

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../../shared/services/auth.service';
 import { FormsModule } from '@angular/forms';
+import { A11yModule } from '@angular/cdk/a11y';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, RouterModule, FormsModule],
+  imports: [CommonModule, RouterModule, FormsModule, A11yModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
@@ -19,6 +20,20 @@ export class LoginComponent {
   login() {
     this.authService
       .login(this.email, this.password)
+      .then((userCredential) => {
+        console.log('Login erfolgreich: ', userCredential.user.email);
+        this.goToApp();
+      })
+      .catch((error) => {
+        console.error('Login fehlgeschlagen: ', error.message);
+      });
+  }
+
+  loginGuest() {
+    const mail = 'gast@email.de';
+    const pw = '0123456';
+    this.authService
+      .login(mail, pw)
       .then((userCredential) => {
         console.log('Login erfolgreich: ', userCredential.user.email);
         this.goToApp();
