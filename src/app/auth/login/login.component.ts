188fa7de8d2058259c3e90f581c6c6024e4489c0
Henry Wolf <HWolf.binz@gmail.com>
2025-11-06 08:58:29 +0100

    add: form validation in login component with error

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -15,31 +15,47 @@ import { A11yModule } from '@angular/cdk/a11y';
 export class LoginComponent {
   email: string = '';
   password: string = '';
+  authError: string | null = null;
+  isSubmitting = false;
   constructor(private router: Router, private authService: AuthService) {}
 
   login() {
+    this.authError = null;
+    this.isSubmitting = true;
     this.authService
       .login(this.email, this.password)
       .then((userCredential) => {
         console.log('Login erfolgreich: ', userCredential.user.email);
         this.goToApp();
+        this.isSubmitting = false;
       })
       .catch((error) => {
         console.error('Login fehlgeschlagen: ', error.message);
+        this.authError = 'Check your email and password. Please try again.';
+        this.isSubmitting = false;
       });
+  }
+
+  clearAuthError() {
+    if (this.authError) this.authError = null;
   }
 
   loginGuest() {
     const mail = 'gast@email.de';
     const pw = '0123456';
+    this.authError = null;
+    this.isSubmitting = true;
     this.authService
       .login(mail, pw)
       .then((userCredential) => {
         console.log('Login erfolgreich: ', userCredential.user.email);
         this.goToApp();
+        this.isSubmitting = false;
       })
       .catch((error) => {
         console.error('Login fehlgeschlagen: ', error.message);
+        this.authError = 'Check your email and password. Please try again.';
+        this.isSubmitting = false;
       });
   }
 
