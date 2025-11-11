812d73d9464d977a8da7064b5952b62cd7eb4883
Nils Alberding-Schulz <nilsalberding@web.de>
2025-11-11 08:40:04 +0100

    add pathMatch: 'full'

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,7 +9,7 @@ export const routes: Routes = [
   // wenn im browser nur / verwendet wird, leitet dies direkt auf /login um
   { path: '', pathMatch: 'full', redirectTo: '/login' },
   // aufruf von /login rendert die Login-Seite
-  { path: 'login', component: LoginComponent },
+  { path: 'login', component: LoginComponent},
 
   // aufruf von /legalAndPrivacy rendert den Inhalt, für die policy oder legal
   { path: 'legalAndPrivacy', component: LegalAndPrivacyComponent },
@@ -18,5 +18,5 @@ export const routes: Routes = [
   // aufruf von /main rendert den Wrapper, der <app-main-content anzeigt
   { path: 'main', component: MainShellComponent, canActivate: [authGuard] },
   // jeder unbekannte pfad wird auf /login umgeleitet
-  { path: '**', redirectTo: '/login' },
+  { path: '**', pathMatch: 'full', redirectTo: '/login' },
 ];
